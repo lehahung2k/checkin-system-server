07903157348724e498874e37e613e72563222c43
lehahung2k <hung2000yt@gmail.com>
2023-04-12 23:52:07 +0700

    config swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Checkin API')
+    .setDescription('Checkin system API endpoints')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('docs/api', app, swaggerDocument);
+
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
